00d7e2d72f838eb0bafec79641a32a725b00d000
jesse <brooks.jesse83@gmail.com>
2024-03-20 19:45:00 -0700

    render updates

--- app/models/cards.py
+++ app/models/cards.py
@@ -1,16 +1,18 @@
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
 class Card(db.Model):
     __tablename__ = 'cards'
 
-    
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
 
     id = db.Column(db.Integer(), primary_key = True)
     image_url = db.Column(db.String(), nullable = False)
     supertype = db.Column(db.String(), nullable = False)
     subtype = db.Column(db.String(), nullable = False)
-    deck_id = db.Column(db.Integer(), db.ForeignKey("decks.id"))
+    deck_id = db.Column(db.Integer(), db.ForeignKey(add_prefix_for_prod("decks.id")))
 
     deck = db.relationship("Deck", back_populates = 'cards')
 
--- app/models/decks.py
+++ app/models/decks.py
@@ -1,12 +1,15 @@
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
 class Deck(db.Model):
     __tablename__ = 'decks'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer(), primary_key = True)
     name = db.Column(db.String(99), nullable = False)
-    user_id = db.Column(db.Integer(), db.ForeignKey("users.id"), nullable = False)
+    user_id = db.Column(db.Integer(), db.ForeignKey(add_prefix_for_prod("users.id")), nullable = False)
 
     cards = db.relationship("Card", back_populates = 'deck', cascade = "all, delete-orphan")
 
